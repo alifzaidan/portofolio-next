0f88b17fbe34f02dbb9efbabc3d343a77975a96c
alifzaidan <zaidanalif22710@gmail.com>
2024-08-17 13:32:10 +0700

    update responsive layout at detail project

--- src/app/projects/[detail]/page.tsx
+++ src/app/projects/[detail]/page.tsx
@@ -9,17 +9,35 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function DetailProject(props: any) {
     const { params } = props;
-    const { data } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/projects?id=${params.detail}`, fetcher);
+    const { data: projectData, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/projects?id=${params.detail}`, fetcher);
+    const { data: allProjects } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`, fetcher); // Mendapatkan semua project
 
     const project = {
-        data: data?.data || [],
+        data: projectData?.data || [],
     };
+    const totalProjects = allProjects?.data?.length || 0;
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-screen bg-black">
+                <div className="text-white">Loading</div>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="flex justify-center items-center h-screen bg-black">
+                <h1 className="text-white">Failed to load data</h1>
+            </div>
+        );
+    }
 
     return (
         <main key={project.data?.id}>
-            <section>
+            <section className="border-b-2 border-black">
                 <div className="border-b-2 border-black bg-gradient-to-b from-[#357B24] to-transparent">
-                    <div className="container md:h-screen flex flex-col justify-center">
+                    <div className="container md:h-screen py-20 flex flex-col justify-center">
                         <h1 className="md:text-8xl text-7xl text-white font-degular font-medium md:mb-28 mb-16">{project.data?.title}</h1>
                         <ul className="space-y-2">
                             <li className="flex items-center gap-4 md:text-xl text-white">
@@ -40,15 +58,18 @@ export default function DetailProject(props: any) {
                         </ul>
                     </div>
                 </div>
-                <div className="container py-24 border-b-2 border-black">
-                    <div className="w-4/5">
-                        <h2 className="text-4xl font-degular text-white bg-[#357B24] inline leading-relaxed">{project.data?.shortDescription}</h2>
+
+                <div className="container md:py-24 py-16">
+                    <div className="md:w-4/5">
+                        <h2 className="md:text-4xl text-2xl font-degular text-white bg-[#357B24] inline leading-relaxed">
+                            {project.data?.shortDescription}
+                        </h2>
                     </div>
-                    <div className="w-2/3">
-                        <h6 className="mt-16 mb-6 text-2xl font-degular font-semibold">Description App</h6>
+                    <div className="md:w-2/3">
+                        <h6 className="md:mt-16 mt-10 md:mb-6 mb-4 text-2xl font-degular font-semibold">Description App</h6>
                         {project.data?.longDescription ? (
                             project.data.longDescription.split('\n').map((paragraph: any, index: any) => (
-                                <p key={index}>
+                                <p key={index} className="text-justify">
                                     {paragraph}
                                     {index < project.data.longDescription.split('\n').length - 1 && (
                                         <>
@@ -63,18 +84,20 @@ export default function DetailProject(props: any) {
                         )}
                     </div>
                 </div>
-                <div className="container h-96 bg-project bg-cover bg-center border-b-2 border-black"></div>
-                <div className="container py-24 border-b-2 border-black">
-                    <div className="grid grid-cols-3 gap-y-24 mb-12">
-                        <div className="font-degular font-semibold text-2xl flex gap-12">
+
+                <div className="h-96 bg-project bg-cover bg-center border-y-2 border-black"></div>
+
+                <div className="container md:py-24 py-12">
+                    <div className="grid lg:grid-cols-3 lg:gap-y-24 md:gap-y-10 gap-y-5 mb-12">
+                        <div className="font-degular font-semibold text-2xl flex md:gap-12 gap-6">
                             <h6 className="text-[#357B24]">01</h6>
                             <h6>Background & Problem</h6>
                         </div>
-                        <div className="col-span-2">
+                        <div className="col-span-2 lg:mb-0 mb-8">
                             <div className="mb-8">
                                 {project.data?.problem?.description ? (
                                     project.data.problem?.description.split('\n').map((paragraph: any, index: any) => (
-                                        <p key={index}>
+                                        <p key={index} className="text-justify">
                                             {paragraph}
                                             {index < project.data.problem?.description.split('\n').length - 1 && (
                                                 <>
@@ -88,32 +111,32 @@ export default function DetailProject(props: any) {
                                     <p>No description available.</p>
                                 )}
                             </div>
-                            <div className="flex gap-5">
+                            <div className="flex md:flex-row flex-col gap-5">
                                 <Image
                                     src={project.data?.problem?.firstImage}
                                     alt={'Project'}
                                     width={1000}
                                     height={1000}
-                                    className="w-full h-72 object-cover"
+                                    className="w-full md:h-72 h-52 object-cover"
                                 />
                                 <Image
                                     src={project.data?.problem?.secondImage}
                                     alt={'Project'}
                                     width={1000}
                                     height={1000}
-                                    className="w-full h-72 object-cover"
+                                    className="w-full md:h-72 h-52 object-cover"
                                 />
                             </div>
                         </div>
-                        <div className="font-degular font-semibold text-2xl flex gap-12">
+                        <div className="font-degular font-semibold text-2xl flex md:gap-12 gap-6">
                             <h6 className="text-[#357B24]">02</h6>
                             <h6>Solution Overview</h6>
                         </div>
-                        <div className="col-span-2">
+                        <div className="col-span-2 lg:mb-0 mb-8">
                             <div className="mb-8">
                                 {project.data?.solution?.description ? (
                                     project.data.solution?.description.split('\n').map((paragraph: any, index: any) => (
-                                        <p key={index}>
+                                        <p key={index} className="text-justify">
                                             {paragraph}
                                             {index < project.data.solution?.description.split('\n').length - 1 && (
                                                 <>
@@ -127,20 +150,20 @@ export default function DetailProject(props: any) {
                                     <p>No description available.</p>
                                 )}
                             </div>
-                            <div className="flex gap-5">
+                            <div className="flex md:flex-row flex-col gap-5">
                                 <Image
                                     src={project.data?.solution?.firstImage}
                                     alt={'Project'}
                                     width={1000}
                                     height={1000}
-                                    className="w-full h-72 object-cover"
+                                    className="w-full md:h-72 h-52 object-cover"
                                 />
                                 <Image
                                     src={project.data?.solution?.secondImage}
                                     alt={'Project'}
                                     width={1000}
                                     height={1000}
-                                    className="w-full h-72 object-cover"
+                                    className="w-full md:h-72 h-52 object-cover object-top"
                                 />
                             </div>
                         </div>
@@ -152,71 +175,101 @@ export default function DetailProject(props: any) {
                             <p className="">{project.data?.keyFeature?.description}</p>
                         </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-y-4">
+                    <div className="grid xl:grid-cols-2 grid-cols-1 xl:gap-y-4 gap-y-6">
                         {project.data?.keyFeature?.features?.map((feature: any, index: any) => (
-                            <div key={index} className={`row-start-${index + 1} col-start-${index % 2 === 0 ? 1 : 2} flex items-center gap-4`}>
-                                {index % 2 === 0 && (
-                                    <Image src={feature.image} alt={feature.title} width={1000} height={1000} className="w-full h-52 object-cover" />
-                                )}
-                                <div className={`space-y-2 ${index % 2 === 0 ? '' : 'text-right'}`}>
-                                    <h6 className="font-bold">{feature.title}</h6>
-                                    <p>{feature.description}</p>
+                            <>
+                                <div
+                                    key={index}
+                                    className={`flex ${
+                                        index % 2 === 0 ? 'md:flex-row xl:col-start-1' : 'xl:flex-row-reverse md:flex-row xl:col-start-2'
+                                    } items-center flex-col-reverse gap-4`}
+                                >
+                                    <Image
+                                        src={feature.image}
+                                        alt={feature.title}
+                                        width={1000}
+                                        height={1000}
+                                        className="w-full h-52 object-contain md:basis-1/2"
+                                    />
+
+                                    <div className={`md:basis-1/2 md:space-y-2 text-center ${index % 2 === 0 ? 'xl:text-left' : 'xl:text-right'}`}>
+                                        <h6 className="font-bold">{feature.title}</h6>
+                                        <p>{feature.description}</p>
+                                    </div>
                                 </div>
-                                {index % 2 !== 0 && (
-                                    <Image src={feature.image} alt={feature.title} width={1000} height={1000} className="w-full h-52 object-cover" />
-                                )}
-                            </div>
+                                <div></div>
+                            </>
                         ))}
                     </div>
                 </div>
-                <div className="container py-24 border-b-2 border-black">
-                    <h2 className="text-4xl font-degular text-white bg-[#357B24] inline leading-relaxed">Technology Stack</h2>
-                    <div>
-                        <ul className="mt-12 grid md:grid-cols-2 grid-cols-1 gap-4">
-                            <li className="flex items-center gap-4 md:text-xl">
-                                <h6>Frontend</h6>
-                                <HiOutlineArrowLongRight />
-                                {project.data?.techStack?.frontend?.length > 0 && (
-                                    <h6 className="font-bold">{project.data.techStack.frontend.join(', ')}</h6>
-                                )}
-                            </li>
-                            <li className="flex items-center gap-4 md:text-xl">
-                                <h6>Backend</h6>
-                                <HiOutlineArrowLongRight />
-                                {project.data?.techStack?.backend?.length > 0 && (
-                                    <h6 className="font-bold">{project.data.techStack.backend.join(', ')}</h6>
-                                )}
-                            </li>
-                            <li className="flex items-center gap-4 md:text-xl">
-                                <h6>API</h6>
-                                <HiOutlineArrowLongRight />
-                                {project.data?.techStack?.api?.length > 0 && <h6 className="font-bold">{project.data.techStack.api.join(', ')}</h6>}
-                            </li>
-                            <li className="flex items-center gap-4 md:text-xl">
-                                <h6>Additional</h6>
-                                <HiOutlineArrowLongRight />
-                                {project.data?.techStack?.additional?.length > 0 && (
-                                    <h6 className="font-bold">{project.data.techStack.additional.join(', ')}</h6>
-                                )}
-                            </li>
-                        </ul>
+
+                <div className="border-t-2 border-black">
+                    <div className="container md:py-24 py-12">
+                        <h2 className="md:text-4xl text-2xl font-degular text-white bg-[#357B24] inline leading-relaxed">Technology Stack</h2>
+                        <div>
+                            <ul className="md:mt-12 mt-6 grid md:grid-cols-2 grid-cols-1 gap-4">
+                                <li className="flex items-center gap-4 md:text-xl">
+                                    <h6>Frontend</h6>
+                                    <HiOutlineArrowLongRight />
+                                    {project.data?.techStack?.frontend?.length > 0 && (
+                                        <h6 className="font-bold">{project.data.techStack.frontend.join(', ')}</h6>
+                                    )}
+                                </li>
+                                <li className="flex items-center gap-4 md:text-xl">
+                                    <h6>Backend</h6>
+                                    <HiOutlineArrowLongRight />
+                                    {project.data?.techStack?.backend?.length > 0 && (
+                                        <h6 className="font-bold">{project.data.techStack.backend.join(', ')}</h6>
+                                    )}
+                                </li>
+                                <li className="flex items-center gap-4 md:text-xl">
+                                    <h6>API</h6>
+                                    <HiOutlineArrowLongRight />
+                                    {project.data?.techStack?.api?.length > 0 && (
+                                        <h6 className="font-bold">{project.data.techStack.api.join(', ')}</h6>
+                                    )}
+                                </li>
+                                <li className="flex items-center gap-4 md:text-xl">
+                                    <h6>Additional</h6>
+                                    <HiOutlineArrowLongRight />
+                                    {project.data?.techStack?.additional?.length > 0 && (
+                                        <h6 className="font-bold">{project.data.techStack.additional.join(', ')}</h6>
+                                    )}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </section>
+
             <footer>
                 <div className="flex justify-between py-6 container ">
-                    <div className="flex items-center gap-4">
-                        <HiOutlineArrowLongLeft className="text-3xl" />
-                        <Link href={'/'} className="font-degular sm:text-3xl text-xl font-medium hover:bg-black hover:text-white transition">
-                            Project 1
-                        </Link>
-                    </div>
-                    <div className="flex items-center gap-4">
-                        <Link href={'/'} className="font-degular sm:text-3xl text-xl font-medium hover:bg-black hover:text-white transition">
-                            Project 3
-                        </Link>
-                        <HiOutlineArrowLongRight className="text-3xl" />
-                    </div>
+                    {params.detail > 1 ? (
+                        <div className="flex items-center gap-4">
+                            <HiOutlineArrowLongLeft className="text-3xl" />
+                            <Link
+                                href={`/projects/${project.data?.id - 1}`}
+                                className="font-degular sm:text-3xl text-xl font-medium hover:bg-black hover:text-white transition"
+                            >
+                                Project {project.data?.id - 1}
+                            </Link>
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
+                    {params.detail < totalProjects ? (
+                        <div className="flex items-center gap-4">
+                            <Link
+                                href={`/projects/${project.data?.id + 1}`}
+                                className="font-degular sm:text-3xl text-xl font-medium hover:bg-black hover:text-white transition"
+                            >
+                                Project {project.data?.id + 1}
+                            </Link>
+                            <HiOutlineArrowLongRight className="text-3xl" />
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
                 </div>
             </footer>
         </main>
